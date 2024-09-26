23fa016f0e41200e48a04fb06a04b4d74ad58897
“vanelozano7312” <vane.lozano7312@gmail.com>
2024-09-26 12:18:03 -0500

    Creation of countFollowers for US0002

--- src/main/java/co/mercadolibre/SocialMeli/controller/SellerController.java
+++ src/main/java/co/mercadolibre/SocialMeli/controller/SellerController.java
@@ -1,7 +1,20 @@
 package co.mercadolibre.SocialMeli.controller;
 
+import co.mercadolibre.SocialMeli.service.ISellerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 public class SellerController {
+    @Autowired
+    ISellerService sellerService;
+
+    @RequestMapping("/users/{userId}/followers/count")
+    public ResponseEntity<?> countFollowers(@PathVariable int userId){
+        return new ResponseEntity<>(sellerService.countFollowers(userId), HttpStatus.OK);
+    }
 }
--- src/main/java/co/mercadolibre/SocialMeli/service/impl/SellerService.java
+++ src/main/java/co/mercadolibre/SocialMeli/service/impl/SellerService.java
@@ -1,4 +1,29 @@
 package co.mercadolibre.SocialMeli.service.impl;
 
-public class SellerService {
+import co.mercadolibre.SocialMeli.dto.response.CountFollowersDTO;
+import co.mercadolibre.SocialMeli.entity.User;
+import co.mercadolibre.SocialMeli.exception.NotFoundException;
+import co.mercadolibre.SocialMeli.repository.impl.UsersRepository;
+import co.mercadolibre.SocialMeli.service.ISellerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SellerService implements ISellerService {
+    @Autowired
+    UsersRepository usersRepository;
+
+    @Override
+    public CountFollowersDTO countFollowers(int userId) {
+        User sellerToCheck = usersRepository.finAllUsers().stream()
+                .filter(user -> user.getUserId() == userId)
+                .findFirst()
+                .orElse(null);
+        if (sellerToCheck == null){
+            throw new NotFoundException("User not found");
+        }
+        return new CountFollowersDTO(sellerToCheck.getUserId(), sellerToCheck.getUserName(), sellerToCheck.getFollowers().size());
+    }
+
+
 }
