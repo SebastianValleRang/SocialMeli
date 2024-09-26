66da81b0e295a7e4d53f79a924d6b810596339d2
agarciaacost_meli <anamaria.garciaacosta@mercadolibre.com.co>
2024-09-26 10:43:59 -0500

    Creacion de controladores

---
+++ src/main/java/co/mercadolibre/SocialMeli/controller/PostController.java
@@ -0,0 +1,7 @@
+package co.mercadolibre.SocialMeli.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostController {
+}
---
+++ src/main/java/co/mercadolibre/SocialMeli/service/impl/User1Service.java
@@ -0,0 +1,37 @@
+package co.mercadolibre.SocialMeli.service.impl;
+
+import co.mercadolibre.SocialMeli.dto.response.ClientFollowedDTO;
+import co.mercadolibre.SocialMeli.dto.response.ResponseDTO;
+import co.mercadolibre.SocialMeli.repository.IUsersRepository;
+import co.mercadolibre.SocialMeli.repository.impl.UsersRepository;
+import co.mercadolibre.SocialMeli.service.IUserService;
+
+public class User1Service implements IUserService {
+
+    IUsersRepository iUsersRepository;
+
+    public User1Service(UsersRepository usersRepository){
+        this.iUsersRepository = usersRepository;
+    }
+
+
+    @Override
+    public ResponseDTO followSeller(int userId, int userIdToFollow) {
+        return null;
+    }
+
+    @Override
+    public ClientFollowedDTO listFollowedSellers(int userId) {
+        return null;
+    }
+
+    @Override
+    public ResponseDTO unfollow(int userId, int userIdToFollow) {
+        return null;
+    }
+
+    @Override
+    public ClientFollowedDTO listFollowedSellersOrder(int userId, String Order) {
+        return null;
+    }
+}
