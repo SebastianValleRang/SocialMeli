ed79cf01962792e586489e9e51c6e575f132a579
SebastianValleRang <sebastianvallejo1999@gmail.com>
2024-09-26 09:27:09 -0500

    Holi

--- src/main/java/co/mercadolibre/SocialMeli/dto/request/PromoPostRequestDTO.java
+++ src/main/java/co/mercadolibre/SocialMeli/dto/request/PromoPostRequestDTO.java
@@ -1,6 +1,7 @@
 package co.mercadolibre.SocialMeli.dto.request;
 
 import co.mercadolibre.SocialMeli.entity.Product;
+import co.mercadolibre.SocialMeli.dto.response.ProductDTO;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.LocalDate;
