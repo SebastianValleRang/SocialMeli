7b6d329f01b5e91acef1c4c079d0d50383c4ad20
SebastianValleRang <sebastianvallejo1999@gmail.com>
2024-09-26 12:18:45 -0500

    Users and products objects upload

--- src/main/java/co/mercadolibre/SocialMeli/repository/impl/UsersRepository.java
+++ src/main/java/co/mercadolibre/SocialMeli/repository/impl/UsersRepository.java
@@ -1,14 +1,55 @@
 package co.mercadolibre.SocialMeli.repository.impl;
 
+import ch.qos.logback.core.net.DefaultSocketConnector;
+import co.mercadolibre.SocialMeli.entity.Post;
+import co.mercadolibre.SocialMeli.entity.Product;
 import co.mercadolibre.SocialMeli.entity.User;
+import co.mercadolibre.SocialMeli.repository.IProductRepository;
 import co.mercadolibre.SocialMeli.repository.IUsersRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-
+@Repository
 public class UsersRepository implements IUsersRepository {
 
+    private IProductRepository iProductRepository;
     private List<User> usersList = new ArrayList<>();
+
+    public UsersRepository(List<User> usersList, ProductRepository productRepository) {
+        this.iProductRepository= productRepository;
+        fillUsers();
+    }
+
+    public void fillUsers(){
+
+        User userJuanPerez = new User(1,"JuanPerez");
+        User userLeandroDiaz = new User(2,"LeandroDiaz");
+        User userAngelaGonzales = new User(3,"AngelaGonzales");
+        User userAnita99 = new User(4,"Anita99");
+        User userFedericoV = new User(5,"FedericoV");
+
+        usersList.add(userJuanPerez);
+        usersList.add(userLeandroDiaz);
+        usersList.add(userAngelaGonzales);
+        usersList.add(userAnita99);
+        usersList.add(userFedericoV);
+
+        Product sillaGamer = iProductRepository.finAllProducts().stream()
+                .filter(p->p.getProductId()==1).findFirst().orElse(null);
+
+        Product almohadaDeChayanne = iProductRepository.finAllProducts().stream()
+                .filter(p->p.getProductId()==4).findFirst().orElse(null);
+
+        userJuanPerez.getPosts().add(new Post(1, userJuanPerez.getUserId(), LocalDate.now(),sillaGamer,1,223.3));
+        userJuanPerez.getPosts().add(new Post(2, userJuanPerez.getUserId(), LocalDate.now(),almohadaDeChayanne,2,227.7));
+
+        System.out.println(userJuanPerez.getPosts());
+
+    }
 
     @Override
     public List<User> finAllUsers() {
