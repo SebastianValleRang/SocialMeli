a672b532c03a0b15d0c5b03e2b40998f68f0a3f4
“vanelozano7312” <vane.lozano7312@gmail.com>
2024-09-27 09:24:28 -0500

    US002 using global methods and its postman request

--- src/main/java/co/mercadolibre/SocialMeli/service/impl/SellerService.java
+++ src/main/java/co/mercadolibre/SocialMeli/service/impl/SellerService.java
@@ -5,20 +5,21 @@ import co.mercadolibre.SocialMeli.entity.User;
 import co.mercadolibre.SocialMeli.exception.NotFoundException;
 import co.mercadolibre.SocialMeli.repository.impl.UsersRepository;
 import co.mercadolibre.SocialMeli.service.ISellerService;
+import co.mercadolibre.SocialMeli.utils.GlobalMethods;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import co.mercadolibre.SocialMeli.utils.GlobalMethods;
 
 @Service
 public class SellerService implements ISellerService {
     @Autowired
     UsersRepository usersRepository;
+    @Autowired
+    GlobalMethods globalMethods;
 
     @Override
     public CountFollowersDTO countFollowers(int userId) {
-        User sellerToCheck = usersRepository.finAllUsers().stream()
-                .filter(user -> user.getUserId() == userId)
-                .findFirst()
-                .orElse(null);
+        User sellerToCheck = globalMethods.getUserById(userId);
         if (sellerToCheck == null){
             throw new NotFoundException("User not found");
         }
