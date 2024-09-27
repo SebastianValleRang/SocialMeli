2bde094f46f7570db960ed73d3d60cc2c0d57b1c
SebastianValleRang <sebastianvallejo1999@gmail.com>
2024-09-27 09:28:41 -0500

    US0010 Done

--- src/main/java/co/mercadolibre/SocialMeli/entity/Post.java
+++ src/main/java/co/mercadolibre/SocialMeli/entity/Post.java
@@ -1,5 +1,6 @@
 package co.mercadolibre.SocialMeli.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,6 +16,7 @@ public class Post {
     private  int postId;
     @JsonProperty("user_id")
     private int userId;
+    @JsonFormat(pattern = "dd-MM-yyyy")
     private LocalDate date;
     private  Product product;
     private int category;
--- src/main/java/co/mercadolibre/SocialMeli/service/impl/PromoPostService.java
+++ src/main/java/co/mercadolibre/SocialMeli/service/impl/PromoPostService.java
@@ -1,4 +1,47 @@
 package co.mercadolibre.SocialMeli.service.impl;
 
-public class PromoPostService {
+import co.mercadolibre.SocialMeli.dto.request.PromoPostRequestDTO;
+import co.mercadolibre.SocialMeli.dto.response.ResponseDTO;
+import co.mercadolibre.SocialMeli.entity.Post;
+import co.mercadolibre.SocialMeli.entity.User;
+import co.mercadolibre.SocialMeli.exception.NotFoundException;
+import co.mercadolibre.SocialMeli.repository.impl.UsersRepository;
+import co.mercadolibre.SocialMeli.service.IPromoPostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PromoPostService implements IPromoPostService {
+    @Autowired
+    UsersRepository usersRepository;
+
+    @Override
+    public ResponseDTO publicPromoPost(PromoPostRequestDTO promoPostRequestDTO) {
+
+        User user = usersRepository.finAllUsers().stream()
+                .filter(p->p.getUserId()==promoPostRequestDTO.getUserId())
+                .findFirst().orElse(null);
+
+        if(user == null){
+            throw new NotFoundException("El usuario suministrado no existe");
+        }
+
+        // Aca va la verificacion del producto
+
+        user.getPosts().add(
+                new Post(
+                        1,
+                        user.getUserId(),
+                        promoPostRequestDTO.getDate(),
+                        promoPostRequestDTO.getProduct(),
+                        promoPostRequestDTO.getCategory(),
+                        promoPostRequestDTO.getPrice(),
+                        promoPostRequestDTO.isHasPromo(),
+                        promoPostRequestDTO.getDiscount()
+                )
+        );
+
+        return new ResponseDTO("Promocion creada: "+promoPostRequestDTO.getProduct().getProductName()+" por "+user.getUserName(), HttpStatus.OK);
+    }
 }
