f30cfbc9f5e4f9329718194aec73908af1122dca
leramirez_meli <leandrojossue.ramirez@mercadolibre.com.co>
2024-09-27 15:15:11 -0500

    fix error from entities

--- src/main/java/co/mercadolibre/SocialMeli/entity/User.java
+++ src/main/java/co/mercadolibre/SocialMeli/entity/User.java
@@ -11,7 +11,9 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+    @JsonProperty("user_id")
     private int userId;
+    @JsonProperty("user_name")
     private String userName;
     @JsonIgnore
     private List<User> followers; //lista de seguidores
