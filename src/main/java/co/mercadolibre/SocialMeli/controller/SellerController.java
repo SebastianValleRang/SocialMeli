4296ae8d0f181440adce631b9ce5942be6411d11
“vanelozano7312” <vane.lozano7312@gmail.com>
2024-09-27 09:54:30 -0500

    Entity correction and GetMapping corretion

--- src/main/java/co/mercadolibre/SocialMeli/controller/SellerController.java
+++ src/main/java/co/mercadolibre/SocialMeli/controller/SellerController.java
@@ -3,8 +3,8 @@ package co.mercadolibre.SocialMeli.controller;
 import co.mercadolibre.SocialMeli.service.ISellerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 
@@ -13,7 +13,7 @@ public class SellerController {
     @Autowired
     ISellerService sellerService;
 
-    @RequestMapping("/users/{userId}/followers/count")
+    @GetMapping("/users/{userId}/followers/count")
     public ResponseEntity<?> countFollowers(@PathVariable int userId){
         return new ResponseEntity<>(sellerService.countFollowers(userId), HttpStatus.OK);
     }
