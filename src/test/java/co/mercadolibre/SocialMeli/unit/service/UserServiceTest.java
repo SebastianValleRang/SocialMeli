9548ab3942536a30b6ada77696de7adf20db9e5a
leramirez_meli <leandrojossue.ramirez@mercadolibre.com.co>
2024-10-09 12:29:45 -0500

    add extra unit tests for T0001 and T0002

--- src/test/java/co/mercadolibre/SocialMeli/unit/service/UserServiceTest.java
+++ src/test/java/co/mercadolibre/SocialMeli/unit/service/UserServiceTest.java
@@ -2,6 +2,7 @@ package co.mercadolibre.SocialMeli.unit.service;
 
 import co.mercadolibre.SocialMeli.dto.response.ResponseDTO;
 import co.mercadolibre.SocialMeli.entity.User;
+import co.mercadolibre.SocialMeli.exception.BadRequestException;
 import co.mercadolibre.SocialMeli.exception.NotFoundException;
 import co.mercadolibre.SocialMeli.repository.IUsersRepository;
 import co.mercadolibre.SocialMeli.service.IUserService;
@@ -80,6 +81,51 @@ class FollowUser{
         verify(globalMethods, never()).isNotSeller(any());
 
     }
+    @DisplayName("T-0001 EXTRA: Follow seller User not found")
+    @Test
+    void followSellerUserDoesntExistsTest(){
+        //Arrange
+
+        User seller = Data.createSeller(2,"Pablo");
+        ResponseDTO expectedResponse = new ResponseDTO("No existe un usuario con el id 1.", HttpStatus.NOT_FOUND);
+
+        //Simulation
+        when(iUsersRepository.findAllUsers()).thenReturn(List.of(seller));
+        when(globalMethods.getUserById(1)).thenReturn(null);
+        when(globalMethods.getUserById(2)).thenReturn(seller);
+
+
+        //Act & Assert
+        NotFoundException notFoundException =assertThrows(NotFoundException.class, () -> userService.followSeller(1,2));
+        assertTrue(notFoundException.getMessage().contains(expectedResponse.getMessage()));
+        verify(iUsersRepository).findAllUsers();
+        verify(globalMethods, times(2)).getUserById(anyInt());
+        verify(globalMethods, never()).isNotSeller(any());
+
+    }
+
+    @DisplayName("T-0001 EXTRA: Follow seller but is not a seller")
+    @Test
+    void followSellerButIsNotASellerTest(){
+        //Arrange
+        User user = Data.createUser(1,"Leandro");
+        User user2 = Data.createUser(2,"Pablo");
+        ResponseDTO expectedResponse = new ResponseDTO("El usuario 2 no es un vendedor.", HttpStatus.NOT_FOUND);
+
+        //Simulation
+        when(iUsersRepository.findAllUsers()).thenReturn(List.of(user,user2));
+        when(globalMethods.getUserById(1)).thenReturn(user);
+        when(globalMethods.getUserById(2)).thenReturn(user2);
+        when(globalMethods.isNotSeller(user2)).thenReturn(true);
+
+        //Act & Assert
+        BadRequestException badRequestException =assertThrows(BadRequestException.class, () -> userService.followSeller(1,2));
+        assertTrue(badRequestException.getMessage().contains(expectedResponse.getMessage()));
+        verify(iUsersRepository).findAllUsers();
+        verify(globalMethods, times(2)).getUserById(anyInt());
+        verify(globalMethods).isNotSeller(user2);
+
+    }
 }
 
 @Nested
@@ -122,6 +168,48 @@ class FollowUser{
         //Act & Assert
         NotFoundException notFoundException =assertThrows(NotFoundException.class, () -> userService.unfollow(1,2));
         assertTrue(notFoundException.getMessage().contains(expectedResponse.getMessage()));
+        verify(iUsersRepository).findAllUsers();
+        verify(globalMethods, times(2)).getUserById(anyInt());
+
+    }
+
+    @DisplayName("T-0002 EXTRA: Unfollow seller User not found")
+    @Test
+    void followSellerUserDoesntExistsTest(){
+        //Arrange
+        User seller = Data.createSeller(2,"Pablo");
+        ResponseDTO expectedResponse = new ResponseDTO("No existe un usuario con el id 1.", HttpStatus.NOT_FOUND);
+
+        //Simulation
+        when(iUsersRepository.findAllUsers()).thenReturn(List.of(seller));
+        when(globalMethods.getUserById(1)).thenReturn(null);
+        when(globalMethods.getUserById(2)).thenReturn(seller);
+
+
+        //Act & Assert
+        NotFoundException notFoundException =assertThrows(NotFoundException.class, () -> userService.unfollow(1,2));
+        assertTrue(notFoundException.getMessage().contains(expectedResponse.getMessage()));
+        verify(iUsersRepository).findAllUsers();
+        verify(globalMethods, times(2)).getUserById(anyInt());
+
+    }
+
+    @DisplayName("T-0002 EXTRA: Unfollow seller but is not a follower")
+    @Test
+    void followSellerButIsNotASellerTest(){
+        //Arrange
+        User user = Data.createUser(1,"Leandro");
+        User user2 = Data.createSeller(2,"Pablo");
+        ResponseDTO expectedResponse = new ResponseDTO("El usuario 1 no es un seguidor de el vendedor 2", HttpStatus.BAD_REQUEST);
+
+        //Simulation
+        when(iUsersRepository.findAllUsers()).thenReturn(List.of(user,user2));
+        when(globalMethods.getUserById(1)).thenReturn(user);
+        when(globalMethods.getUserById(2)).thenReturn(user2);
+
+        //Act & Assert
+        BadRequestException badRequestException =assertThrows(BadRequestException.class, () -> userService.unfollow(1,2));
+        assertTrue(badRequestException.getMessage().contains(expectedResponse.getMessage()));
         verify(iUsersRepository).findAllUsers();
         verify(globalMethods, times(2)).getUserById(anyInt());
 
