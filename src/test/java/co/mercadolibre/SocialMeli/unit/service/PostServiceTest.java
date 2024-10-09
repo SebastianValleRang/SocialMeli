f5f712f3cd2a0d67fb2813bf2e328f2f11e80819
“vanelozano7312” <vane.lozano7312@gmail.com>
2024-10-09 10:03:32 -0500

    Creation of the test T0008

--- src/test/java/co/mercadolibre/SocialMeli/unit/service/PostServiceTest.java
+++ src/test/java/co/mercadolibre/SocialMeli/unit/service/PostServiceTest.java
@@ -1,12 +1,69 @@
 package co.mercadolibre.SocialMeli.unit.service;
 
+import co.mercadolibre.SocialMeli.dto.ProductDTO;
+import co.mercadolibre.SocialMeli.dto.response.PostResponseDTO;
+import co.mercadolibre.SocialMeli.dto.response.RecentPostDTO;
+import co.mercadolibre.SocialMeli.entity.Post;
+import co.mercadolibre.SocialMeli.entity.Product;
+import co.mercadolibre.SocialMeli.entity.User;
+import co.mercadolibre.SocialMeli.repository.IUsersRepository;
+import co.mercadolibre.SocialMeli.service.impl.PostService;
 import co.mercadolibre.SocialMeli.util.Data;
+import co.mercadolibre.SocialMeli.utils.GlobalMethods;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class PostServiceTest {
-    //Arrange
-    //Act
-    //Assert
+    @Mock
+    private IUsersRepository usersRepository;
+    @Mock
+    private GlobalMethods globalMethods;
+    @Mock
+    ObjectMapper mapper;
+
+    @InjectMocks
+    private PostService postService;
+
+    @Nested
+    class showPostLastTwoWeeks{
+        @DisplayName("T0008: Verificar que los post son unicamente de las últimas 2 semanas ")
+        @Test
+        void getPostsFromFollowedLastTwoWeeksTest() {
+            //Arrange
+            User client = Data.getUserThatFollows1Seller();
+            int clientId = client.getUserId();
+
+            Product product = new Product(1,"Mesedora","Muebles","Sillas jairo","Blanco", "Realizada con madera de roble");
+            ProductDTO productDTO = new ProductDTO(1,"Mesedora","Muebles","Sillas jairo","Blanco", "Realizada con madera de roble");
+
+            Post post = new Post(1, 2, LocalDate.parse("2024-10-03"), product,1,223.3);
+            PostResponseDTO postDTO = new PostResponseDTO(1, 2, LocalDate.parse("2024-10-03"), productDTO,1,223.3);
+
+            List<PostResponseDTO> expectedPosts = List.of(postDTO);
+
+            //Act
+            when(usersRepository.findAllUsers()).thenReturn(List.of(client));
+            when(globalMethods.getUserById(clientId)).thenReturn(client);
+            when(mapper.convertValue(post, PostResponseDTO.class)).thenReturn(postDTO);
+
+            RecentPostDTO found = postService.getPostsByFollowedUsersLastTwoWeeks(clientId, null);
+
+            //Assert
+            Assertions.assertEquals(clientId, found.getUserId());
+            Assertions.assertEquals(expectedPosts, found.getRecentPosts());
+        }
+    }
 }
--- src/test/java/co/mercadolibre/SocialMeli/util/Data.java
+++ src/test/java/co/mercadolibre/SocialMeli/util/Data.java
@@ -2,8 +2,35 @@ package co.mercadolibre.SocialMeli.util;
 
 
 import co.mercadolibre.SocialMeli.dto.request.PostRequestDTO;
+import co.mercadolibre.SocialMeli.entity.Post;
+import co.mercadolibre.SocialMeli.entity.Product;
+import co.mercadolibre.SocialMeli.entity.User;
+
+import java.time.LocalDate;
 
 public class Data {
 //public static final Objeto NOMBRE = new Objeto();
+
+    public static User getUserThatFollows1Seller(){
+        User client = new User(1, "Cliente");
+
+        Product product1 = new Product(1,"Mesedora","Muebles","Sillas jairo","Blanco", "Realizada con madera de roble");
+        Product product2 = new Product(2,"Licuadora","Cocina","Imusa","Negro","Gomela");
+        Product product3 = new Product(3,"Nevera","Cocina","Samsung","Blanco","Con congelador");
+
+        User seller = new User(2, "Vendedor1");
+        Post post1 = new Post(1, 2, LocalDate.parse("2024-10-03"), product1,1,223.3);
+        Post post2 = new Post(2, 2, LocalDate.parse("2024-09-23"), product2, 2, 150.5);
+        Post post3 = new Post(3, 2, LocalDate.parse("2023-01-01"), product3, 2, 525.0);
+
+        seller.getPosts().add(post1);
+        seller.getPosts().add(post2);
+        seller.getPosts().add(post3);
+
+        seller.getFollowers().add(client);
+        client.getFollowed().add(seller);
+
+        return client;
+    }
 
 }
