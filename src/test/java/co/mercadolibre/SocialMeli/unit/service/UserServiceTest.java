2252f3ec86c0478b15a3d4d0c53b0688f811fea6
SebastianValleRang <sebastianvallejo1999@gmail.com>
2024-10-09 11:23:56 -0500

    T0003 Done

--- src/test/java/co/mercadolibre/SocialMeli/unit/service/UserServiceTest.java
+++ src/test/java/co/mercadolibre/SocialMeli/unit/service/UserServiceTest.java
@@ -1,12 +1,13 @@
 package co.mercadolibre.SocialMeli.unit.service;
 
 import co.mercadolibre.SocialMeli.dto.response.ClientFollowedDTO;
+import co.mercadolibre.SocialMeli.dto.response.SellerFollowersDTO;
 import co.mercadolibre.SocialMeli.entity.User;
+import co.mercadolibre.SocialMeli.exception.NotFoundException;
 import co.mercadolibre.SocialMeli.repository.IUsersRepository;
-import co.mercadolibre.SocialMeli.repository.impl.UsersRepository;
-import co.mercadolibre.SocialMeli.service.IUserService;
+import co.mercadolibre.SocialMeli.service.impl.SellerService;
 import co.mercadolibre.SocialMeli.service.impl.UserService;
-import co.mercadolibre.SocialMeli.util.TestUtilsFactory;
+import co.mercadolibre.SocialMeli.util.Data;
 import co.mercadolibre.SocialMeli.utils.GlobalMethods;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -17,46 +18,168 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
 
     @Mock
-    UsersRepository usersRepository;
+    IUsersRepository iUsersRepository;
     @Mock
     GlobalMethods globalMethods;
 
     @InjectMocks
     UserService userService;
+    @InjectMocks
+    SellerService sellerService;
+
+
 
     @Nested
     class T0003{
-        @DisplayName("Permite continuar con normalidad.")
+        @DisplayName("Ordenar seguidos - Camino Bueno")
         @Test
-        void orderingExist(){
+        void orderingFollowedGood(){
 
             //Arrange
             int userId = 1;
-            String order = "name_asc";
-
-            List<User> usersList = TestUtilsFactory.getUsersListTest();
+            String order = null;
+            List<User> usersList = Data.getUsersListTest();
             User userById = usersList.stream().filter(u->u.getUserId() == userId).findFirst().orElse(null);
-
-            ClientFollowedDTO expectedJson = TestUtilsFactory.getlistFollowedSellersTest();
+            ClientFollowedDTO expectedJson = Data.getlistFollowedSellersTest();
 
             //Act
-            when(usersRepository.findAllUsers()).thenReturn(usersList);
+            when(iUsersRepository.findAllUsers()).thenReturn(usersList);
             when(globalMethods.getUserById(userId)).thenReturn(userById);
 
             ClientFollowedDTO serviceResponse = userService.listFollowedSellers(userId, order);
 
             //Assert
             Assertions.assertEquals(expectedJson,serviceResponse);
+            verify(globalMethods).getUserById(anyInt());
+            verify(iUsersRepository).findAllUsers();
 
         }
+
+        @DisplayName("Ordenar seguidos - Camino Malo, no hay usuarios")
+        @Test
+        void orderingFollowedBadUsers(){
+
+            //Arrange
+            int userId = 1;
+            String order = null;
+            List<User> usersList = new ArrayList<>();
+
+            //Act & Assert
+            when(iUsersRepository.findAllUsers()).thenReturn(usersList);
+
+            NotFoundException notFoundException = Assertions.assertThrows(NotFoundException.class, () -> {
+                userService.listFollowedSellers(userId, order);
+            });
+
+            Assertions.assertEquals("No hay usuarios registrados", notFoundException.getMessage());
+            verify(globalMethods, never()).getUserById(anyInt());
+            verify(iUsersRepository).findAllUsers();
+
+        }
+
+        @DisplayName("Ordenar seguidos - Camino Malo, no encuentra el usuario")
+        @Test
+        void orderingFollowedBadFindUser(){
+
+            //Arrange
+            int userId = 1;
+            String order = null;
+            List<User> usersList = Data.getUsersListTest();
+
+            //Act & Assert
+            when(iUsersRepository.findAllUsers()).thenReturn(usersList);
+            when(globalMethods.getUserById(userId)).thenReturn(null);
+
+            NotFoundException notFoundException = Assertions.assertThrows(NotFoundException.class, () -> {
+                userService.listFollowedSellers(userId, order);
+            });
+
+            Assertions.assertEquals("Usuario con el id %d no se ha encontrado.".formatted(userId) , notFoundException.getMessage());
+            verify(iUsersRepository).findAllUsers();
+            verify(globalMethods).getUserById(anyInt());
+
+        }
+
+        @DisplayName("Ordenar seguidores - Camino Bueno")
+        @Test
+        void orderingFollowersGood(){
+
+            //Arrange
+            int userId = 1;
+            String order = null;
+            List<User> usersList = Data.getUsersListTest();
+            User userById = usersList.stream().filter(u->u.getUserId() == userId).findFirst().orElse(null);
+            SellerFollowersDTO expectedJson = Data.getlistFollowersSellersTest();
+
+            //Act
+            when(globalMethods.getUserById(userId)).thenReturn(userById);
+            when(globalMethods.isNotSeller(userById)).thenReturn(false);
+
+            SellerFollowersDTO serviceResponse = sellerService.listFollowers(userId, order);
+
+            //Assert
+            Assertions.assertEquals(expectedJson,serviceResponse);
+            verify(globalMethods).getUserById(anyInt());
+            verify(globalMethods).isNotSeller(any(User.class));
+
+        }
+
+        @DisplayName("Ordenar seguidores - Camino Malo, no encuentra el usuario")
+        @Test
+        void orderingFollowersBadUsers(){
+
+            //Arrange
+            int userId = 1;
+            String order = null;
+            User userById = null;
+
+            //Act & Assert
+            when(globalMethods.getUserById(userId)).thenReturn(userById);
+
+            NotFoundException notFoundException = Assertions.assertThrows(NotFoundException.class, () -> {
+                sellerService.listFollowers(userId, order);
+            });
+
+            Assertions.assertEquals("El usuario con el id %d no se ha encontrado".formatted(userId) , notFoundException.getMessage());
+            verify(globalMethods).getUserById(anyInt());
+            verify(globalMethods, never()).isNotSeller(any(User.class));
+
+        }
+
+        @DisplayName("Ordenar seguidores - Camino Malo, el usuario no es vendedor")
+        @Test
+        void orderingFollowersBadFindUser(){
+
+            //Arrange
+            int userId = 1;
+            String order = null;
+            List<User> usersList = Data.getUsersListTest();
+            User userById = usersList.stream().filter(u->u.getUserId() == userId).findFirst().orElse(null);
+
+            //Act & Assert
+            when(globalMethods.getUserById(userId)).thenReturn(userById);
+            when(globalMethods.isNotSeller(userById)).thenReturn(true);
+
+            NotFoundException notFoundException = Assertions.assertThrows(NotFoundException.class, () -> {
+                sellerService.listFollowers(userId, order);
+            });
+
+            Assertions.assertEquals("El usuario con el id %d no es un vendedor".formatted(userId) , notFoundException.getMessage());
+            verify(globalMethods).getUserById(anyInt());
+            verify(globalMethods).isNotSeller(any(User.class));
+
+        }
+
     }
 
 }
--- src/test/java/co/mercadolibre/SocialMeli/util/Data.java
+++ src/test/java/co/mercadolibre/SocialMeli/util/Data.java
@@ -1,9 +1,77 @@
 package co.mercadolibre.SocialMeli.util;
 
+import co.mercadolibre.SocialMeli.dto.response.ClientFollowedDTO;
+import co.mercadolibre.SocialMeli.dto.response.SellerFollowersDTO;
+import co.mercadolibre.SocialMeli.dto.response.UserDTO;
+import co.mercadolibre.SocialMeli.entity.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import co.mercadolibre.SocialMeli.dto.request.PostRequestDTO;
+import java.util.List;
 
 public class Data {
-//public static final Objeto NOMBRE = new Objeto();
+    static ObjectMapper objectMapper = new ObjectMapper();
+
+    public static List<User> getUsersListTest() {
+
+        List<User> usersList;
+
+        User userJuanPerez = new User(1, "JuanPerez");
+        User userLeandroDiaz = new User(2, "LeandroDiaz");
+        User userAngelaGonzales = new User(3, "AngelaGonzales");
+        User userAnita99 = new User(4, "Anita99");
+        User userFedericoV = new User(5, "FedericoV");
+
+        usersList = List.of(userJuanPerez, userLeandroDiaz, userAngelaGonzales,
+                userAnita99, userFedericoV);
+
+        userJuanPerez.getFollowers().add(userLeandroDiaz);
+        userJuanPerez.getFollowers().add(userAngelaGonzales);
+
+        userJuanPerez.getFollowed().add(userFedericoV);
+        userJuanPerez.getFollowed().add(userAnita99);
+
+        return usersList;
+
+    }
+
+    public static ClientFollowedDTO getlistFollowedSellersTest() {
+
+        User userJuanPerez = new User(1, "JuanPerez");
+        User userAnita99 = new User(4, "Anita99");
+        User userFedericoV = new User(5, "FedericoV");
+
+        userJuanPerez.getFollowed().add(userFedericoV);
+        userJuanPerez.getFollowed().add(userAnita99);
+
+        ClientFollowedDTO clientFollowedDTO = new ClientFollowedDTO(
+                userJuanPerez.getUserId(),
+                userJuanPerez.getUserName(),
+                List.of(objectMapper.convertValue(userFedericoV, UserDTO.class)
+                        ,objectMapper.convertValue(userAnita99,UserDTO.class))
+        );
+
+        return clientFollowedDTO;
+
+    }
+
+    public static SellerFollowersDTO getlistFollowersSellersTest() {
+
+        User userJuanPerez = new User(1, "JuanPerez");
+        User userLeandroDiaz = new User(2, "LeandroDiaz");
+        User userAngelaGonzales = new User(3, "AngelaGonzales");
+
+        userJuanPerez.getFollowers().add(userLeandroDiaz);
+        userJuanPerez.getFollowers().add(userAngelaGonzales);
+
+        SellerFollowersDTO sellerFollowersDTO = new SellerFollowersDTO(
+                userJuanPerez.getUserId(),
+                userJuanPerez.getUserName(),
+                List.of(objectMapper.convertValue(userLeandroDiaz,UserDTO.class)
+                        ,objectMapper.convertValue(userAngelaGonzales,UserDTO.class))
+        );
+
+        return sellerFollowersDTO;
+
+    }
 
 }
